b2549945c267a9e3418f4bbfeff5e67274d6aaaa
Zak Ali <zak.ali@thetin.net>
2020-12-28 19:05:23 +0000

    Add basic route structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react"
+import {Route, Switch} from "react-router-dom"
+import Home from "./Home"
+import Players from "./Players"
+import Teams from "./Teams"
+import Navbar from "./Navbar"
 
 function App() {
   return (
-    <div className="">
-
+    <div className="container">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/players" exact component={Players} />
+        <Route path="/teams" exact component={Teams} />
+      </Switch>
     </div>
   );
 }
